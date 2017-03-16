e0a1fdbb1e263fb6a8cb19781d6a5cb619375404
Christophe Gagnier <admin@cgagnier.ca>
2017-03-16 00:17:12 -0400

    Add SQL schema for the database test

--- lib/controllers/index.php
+++ lib/controllers/index.php
@@ -13,17 +13,16 @@ class index extends baseController {
 
     public function testDatabase(Request $request, Response $response) {
         $name = $request->getAttribute('name');
-        $response->getBody()->write("Hello, $name");
+        $response->getBody()->write("<h1>Database test</h1>");
 
         $result = $this->app->db->query('SELECT * FROM test')->fetchAll();
 
         foreach ($result as $resultRow) {
-            $response->getBody()->write("<br><br>");
             $response->getBody()->write("{$resultRow['id']} => {$resultRow['value']}");
+            $response->getBody()->write("<br>");
         }
 
 
         return $response;
-
     }
 }
