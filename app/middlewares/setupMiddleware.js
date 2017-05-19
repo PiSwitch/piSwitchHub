faff3695971170c8c0befeed92eb3d3184b326ad
Christophe Gagnier <admin@cgagnier.ca>
2017-05-19 00:46:31 -0400

    #5 Create a setup to configure the hub

--- app/middlewares/setupMiddleware.js
+++ app/middlewares/setupMiddleware.js
@@ -1,10 +1,11 @@
 exports = {};
 var masterServer = require('../models/masterServer');
+var sql = require('../lib/sqlPool');
 
 //route middleware to make sure that the app has been setup
 exports.mustBeSetup = function(req, res, next) {
     //TODO: Write condition to check if the setup was done
-    if (!masterServer.isSetup() && false)
+    if (masterServer.isSetup() && sql.isSetup())
         return next();
     res.redirect('/setup/step_one')
 };
